f449abc49f60b9cb314ac02e46b131f8dc900573
Foyth44 <100964688+arssedot@users.noreply.github.com>
2026-03-22 01:02:01 +0300

    updating the functionality of the dashboard controller

--- src/main/java/com/arssedot/loganalyzer/web/controller/DashboardController.java
+++ src/main/java/com/arssedot/loganalyzer/web/controller/DashboardController.java
@@ -1,24 +1,13 @@
 package com.arssedot.loganalyzer.web.controller;
 
-import com.arssedot.loganalyzer.service.LogQueryService;
-import com.arssedot.loganalyzer.web.dto.LogFilterDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-@RequiredArgsConstructor
 public class DashboardController {
 
-    private final LogQueryService logQueryService;
-
     @GetMapping("/")
-    public String dashboard(@ModelAttribute LogFilterDto filter, Model model) {
-        model.addAttribute("logs", logQueryService.search(filter));
-        model.addAttribute("filter", filter);
-        model.addAttribute("services", logQueryService.getDistinctServices());
+    public String dashboard() {
         return "dashboard";
     }
 }
